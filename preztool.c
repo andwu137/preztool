35462472f1ed90ea4a8d72a841de2402926eb233
Andrew Nguyen <nguyenandrew137@protonmail.com>
2025-01-04 15:49:48 -0500

    feat: add the ability to mirror the image in X/Y

--- preztool.c
+++ preztool.c
@@ -4,23 +4,28 @@
 #include <rlgl.h>
 #include <stdlib.h>
 
+enum prez_flags {
+  FLAGS_MIRROR_X = 1 << 0,
+  FLAGS_MIRROR_Y = 1 << 1,
+};
+
 int main(int argc, char *argv[]) {
   // screenshot
   unsigned char *data;
-  int dataWidth;
-  int dataHeight;
-  screenshot(&data, &dataWidth, &dataHeight);
+  int srcWidth;
+  int srcHeight;
+  screenshot(&data, &srcWidth, &srcHeight);
 
   // start window
   SetConfigFlags(FLAG_WINDOW_UNDECORATED | FLAG_WINDOW_TOPMOST |
                  FLAG_BORDERLESS_WINDOWED_MODE | FLAG_MSAA_4X_HINT);
-  InitWindow(dataWidth, dataHeight, "preztool");
+  InitWindow(srcWidth, srcHeight, "preztool");
   SetWindowPosition(0, 0);
   SetTargetFPS(60);
 
   // load screenshot as texture
-  Image img = {.width = dataWidth,
-               .height = dataHeight,
+  Image img = {.width = srcWidth,
+               .height = srcHeight,
                .data = data,
                .format = PIXELFORMAT_UNCOMPRESSED_R8G8B8A8,
                .mipmaps = 1};
@@ -28,41 +33,79 @@ int main(int argc, char *argv[]) {
   UnloadImage(img);
 
   /* prez data */
+  unsigned char prezFlags = 0;
   // camera
   Camera2D camera = {};
   camera.zoom = 1.0f;
   // draw
-  RenderTexture2D drawTarget = LoadRenderTexture(dataWidth, dataHeight);
+  RenderTexture2D drawTarget = LoadRenderTexture(srcWidth, srcHeight);
   Color drawColors[] = {RED, GREEN, BLUE, WHITE, BLACK};
   int drawColorSelected = 0;
   // mouse
-  Vector2 mousePos = GetMousePosition();
+  Vector2 mousePos;
   Vector2 prevMousePos;
+  Vector2 prevMouseWorldPos;
+  Vector2 mouseWorldPos;
 
   while (!WindowShouldClose()) {
+    // swap flags
+    if (IsKeyPressed(KEY_A)) {
+      prezFlags ^= FLAGS_MIRROR_X;
+
+      // for prev
+      mousePos.x = srcWidth - mousePos.x;
+      mouseWorldPos.x = srcWidth - mouseWorldPos.x;
+    }
+    if (IsKeyPressed(KEY_S)) {
+      prezFlags ^= FLAGS_MIRROR_Y;
+
+      // for prev
+      mousePos.y = srcHeight - mousePos.y;
+      mouseWorldPos.y = srcHeight - mouseWorldPos.y;
+    }
+
+    // mouse init
+    Vector2 flipVector = Vector2One();
     prevMousePos = mousePos;
     mousePos = GetMousePosition();
-    Vector2 prevMouseWorldPos = GetScreenToWorld2D(prevMousePos, camera);
-    Vector2 mouseWorldPos = GetScreenToWorld2D(mousePos, camera);
+    prevMouseWorldPos = mouseWorldPos;
+    mouseWorldPos = GetScreenToWorld2D(mousePos, camera);
+
+    Vector2 delta = GetMouseDelta();
+    float wheel = GetMouseWheelMove();
+
+    // flip
+    if (prezFlags & FLAGS_MIRROR_X) {
+      mousePos.x = srcWidth - mousePos.x;
+      mouseWorldPos.x = srcWidth - mouseWorldPos.x;
+      flipVector.x = -1;
+    }
+    if (prezFlags & FLAGS_MIRROR_Y) {
+      mousePos.y = srcHeight - mousePos.y;
+      mouseWorldPos.y = srcHeight - mouseWorldPos.y;
+      flipVector.y = -1;
+    }
+    Vector2Multiply(delta, flipVector);
 
     // translate
     if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
-      Vector2 delta = GetMouseDelta();
       delta = Vector2Scale(delta, -1.0f / camera.zoom);
       camera.target = Vector2Add(camera.target, delta);
     }
 
     // zoom
-    float wheel = GetMouseWheelMove();
     if (wheel != 0) {
       // cursor relation
+      Vector2 mousePos = GetMousePosition();
+      Vector2 mouseWorldPos = GetScreenToWorld2D(mousePos, camera);
       camera.offset = mousePos;
       camera.target = mouseWorldPos;
 
-      // zoom
+      // set
       float scaleFactor = 1.0f + (0.25f * fabsf(wheel));
-      if (wheel < 0)
+      if (wheel < 0) {
         scaleFactor = 1.0f / scaleFactor;
+      }
       camera.zoom = Clamp(camera.zoom * scaleFactor, 0.125f, 64.0f);
     }
 
@@ -98,25 +141,21 @@ int main(int argc, char *argv[]) {
     // render
     BeginDrawing();
     {
-      ClearBackground(WHITE);
+      ClearBackground(BLACK);
 
       BeginMode2D(camera);
       {
-        rlPushMatrix();
-        {
-          rlTranslatef(0, 25 * 50, 0);
-          rlRotatef(90, 1, 0, 0);
-        }
-        rlPopMatrix();
-        DrawTexture(screenTexture, 0, 0, WHITE);
+        DrawTextureRec(screenTexture,
+                       (Rectangle){0, 0, srcWidth * flipVector.x,
+                                   srcHeight * flipVector.y},
+                       (Vector2){0, 0}, WHITE);
         DrawTextureRec(drawTarget.texture,
-                       (Rectangle){0, 0, (float)drawTarget.texture.width,
-                                   (float)-drawTarget.texture.height},
+                       (Rectangle){0, 0,
+                                   drawTarget.texture.width * flipVector.x,
+                                   -drawTarget.texture.height * flipVector.y},
                        (Vector2){0, 0}, WHITE);
       }
       EndMode2D();
-
-      DrawFPS(0, 0);
     }
     EndDrawing();
   }
