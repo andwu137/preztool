34e5e5195848864b318e2db433443f061ae07dcb
Andrew Nguyen <nguyenandrew137@protonmail.com>
2025-11-30 18:47:27 -0500

    feat: make the rolloff of the flashlight sharper

--- assets/shaders/flashlight.frag.glsl
+++ assets/shaders/flashlight.frag.glsl
@@ -16,16 +16,17 @@ uniform light flashlight;
 void main() {
     float d = distance(gl_FragCoord.xy, flashlight.pos);
     float alpha = flashlight.outerAlpha;
+    vec3 color = flashlight.color;
 
     if (d > flashlight.outer) {
         alpha = flashlight.outerAlpha;
     } else if (d < flashlight.inner) {
         alpha = flashlight.innerAlpha;
     } else {
-        alpha = mix(flashlight.innerAlpha,
-                    flashlight.outerAlpha,
-                    (d - flashlight.inner) / (flashlight.outer - flashlight.inner));
+        float strength = (d - flashlight.inner) / (flashlight.outer - flashlight.inner);
+        float strengthSquared = strength * strength; // TODO(andrew): look into making this exp?
+        alpha = mix(flashlight.innerAlpha, flashlight.outerAlpha, strengthSquared);
     }
 
-    finalColor = vec4(flashlight.color, alpha);
+    finalColor = vec4(color, alpha);
 }
